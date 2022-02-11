b58e37c6d906cdd13bd0133e49acb67c61f18523
Naik-Akshata <akshtanaik2000@gmail.com>
2022-02-11 15:39:36 +0530

    BranchTest

--- Akshta_cmd.js
+++ Akshta_cmd.js
@@ -3,11 +3,8 @@
 // 3) git commit -m "Your Personal Comments"
 // 4) git pull
 // 5) git push
-<<<<<<< HEAD
 //ADDED NEW COMMENT HERE
-=======
 
 
-
+//CHANGES IN AKSHATA BRANCH
 // Add by the Akshay Pawar
->>>>>>> 02b19bd1e422f0d899c27e5976f666cebab0975e
